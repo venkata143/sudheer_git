8bbe83a0d564169147d958e5a3f64c185f75c8dc
EC2 Default User <ec2-user@ip-172-31-43-163.ap-south-1.compute.internal>
2020-10-18 06:15:04 +0000

    1st line

--- HelloDevops.java
+++ HelloDevops.java
@@ -1,6 +1,7 @@
 public class HelloDevops{
  public static void main(String args[]){
- 	 System.out.println("Poll SCM validation");  
+ 	 System.out.println("Poll SCM validation");
+       System.out.println("ths is 1st line");	 
 	 System.out.println("Welcome to Devops World!");
  }
 
