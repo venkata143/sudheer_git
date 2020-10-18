a960a5b661f41d61e22669248470f7ed2a9f68fe
EC2 Default User <ec2-user@ip-172-31-43-163.ap-south-1.compute.internal>
2020-10-18 09:32:07 +0000

    wehook stmt added

--- HelloDevops.java
+++ HelloDevops.java
@@ -4,6 +4,7 @@ public class HelloDevops{
        System.out.println("ths is 1st linie");	 
 	 System.out.println("Welcome to Devops World!");
 	 System.out.println("Enterd a new a line");
+	 System.out.println("webhook practice");
  }
 
 }
